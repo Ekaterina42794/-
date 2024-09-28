74efbabca188568da11c8ce81cddd773afe7111f
Катя <katja48tab42794@gmail.com>
2024-09-28 18:03:18 +0300

    базовые структуры новое задание
    Signed-off-by: Катя <katja48tab42794@gmail.com>

---
+++ Переменные_прз.py
@@ -0,0 +1,8 @@
+Vsego_DZ=12
+All_time=1.5
+Kurs=("Pyton")
+tdz=All_time/Vsego_DZ
+print(tdz)
+print("Курс:",Kurs,", всего задач:",Vsego_DZ,",")
+print("затрачено часов:",All_time,",")
+print("среднее время выполнения:",tdz,"часа или",tdz*60,"минут")
