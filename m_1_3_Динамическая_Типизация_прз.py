eff1f2e983a6c29ce7ed896eba1133ef270fc579
Катя <katja48tab42794@gmail.com>
2024-10-02 12:17:16 +0300

    новый
    Signed-off-by: Катя <katja48tab42794@gmail.com>

--- m_1_3_Динамическая_Типизация_прз.py
+++ m_1_3_Динамическая_Типизация_прз.py
@@ -1,3 +1,17 @@
+print("Практ.раб. по теме Динамическая типизация")
+
+print("module_1_3")
+name ="Катя"              # - Создайте переменную name и присвойте ей значение вашего имени (строка)
+print(name, type(name))   # Выведите значение переменной name на экран.
+age=41                    #- Создайте переменную age и присвойте ей значение вашего возраста (целое число).
+print(age, type(age))     # Выведите значение переменной age на экран.
+age=age+4                 # Перезапишите в age текущее значение переменной age + новое.
+print(age, type(age))     # - Выведите измененное значение переменной age на экран.
+is_student=True           #- Создайте переменную is_student и присвойте ей значение True (логическое значение).
+print(is_student, type(is_student))        #- Выведите значение переменной is_student на экран.
+print( )
+print( )
+print("module_1_3")
 name ="Катя"
 print("Name:",name, type(name))
 age=41
@@ -5,5 +19,4 @@ print("Age:",age, type(age))
 age=age+4
 print("New Age:",age, type(age))
 is_student=True
-print("is_student",is_student, type(is_student))
-
+print("is_student: ",is_student, type(is_student))
