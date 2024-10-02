9631262d34c949a65e1046b25b9ca038de64ad49
Катя <katja48tab42794@gmail.com>
2024-10-02 12:12:51 +0300

    новый
    Signed-off-by: Катя <katja48tab42794@gmail.com>

--- module_1_4.py
+++ module_1_4.py
@@ -4,6 +4,7 @@ print("Количество символов=",len(my_string))
 print("в верхнем регистре",my_string.upper())      #Выведите строку my_string в верхнем регистре.
 print("в нижнем регистре", my_string.lower())      #Выведите строку my_string в нижнем регистре.
 print("удалив все пробелы", my_string.replace(" ","") )  #Измените строку my_string, удалив все пробелы.
-print("первый символ: :, my_string[0])           #Выведите первый символ строки my_string.
+print("первый символ: ", my_string[0])  #Выведите первый символ строки my_string.
 print(my_string[-1])   #Выведите последний символ строки my_string.
+
 
