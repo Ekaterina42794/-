cfae7590714cb48c1dce72d9df0404e4867c431e
Катя <katja48tab42794@gmail.com>
2024-10-12 16:54:54 +0300

    12/10/2024
    Signed-off-by: Катя <katja48tab42794@gmail.com>

--- Средний балл.py
+++ Средний балл.py
@@ -19,6 +19,8 @@
 Самостоятельно составлять (вручную) словарь не нужно (только изначально пустой).
 Для решения задачи нужно вспомнить функции sum, len и др. (подумать самому).
 Помните, что множество не является упорядоченной последовательностью. (нужен перевод в другой тип).'''
+
+
 print('Дано')
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 print('оценки в алфавитном порядке',type(grades),grades)
@@ -28,6 +30,11 @@ students=list(students)
 print(type(students),(students))
 students=sorted(students)
 print(type(students),(students))
-#my_dict=['1':1]
-#    my_dict.update({'grades(1)':'students(1)','grades(2)':'students(2)'}))# ОШИБКА
-#print(my_dict)
+grades[0]=sum(grades[0])/len (grades[0])
+grades[1]=sum(grades[1])/len (grades[1])
+grades[2]=sum(grades[2])/len (grades[2])
+grades[3]=sum(grades[3])/len (grades[3])
+grades[4]=sum(grades[4])/len (grades[4])
+print(grades)
+ср_балл={key:value for(key,value)in zip(students,grades)}
+print('Средний балл_', ср_балл)
